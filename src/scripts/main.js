2665fa70a0214f60c2c8a7927df0ed77d78d8b7e
Ingo Steinke <ingosteinke@gmx.de>
2021-12-14 08:23:54 +0100

    add customer webpack configuration

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+import './globals';
+
+// import javascript from all components (using import-glob as a preloader before eslint-loader)
+// eslint-disable-next-line import/extensions, import/no-unresolved
+import '../components/**/*.js';
+
+/** *** */ (() => {
+  // webpackBootstrap
+  /** *** */
+  /** *** */
+  /** *** */
+  /** *** */
+})();
